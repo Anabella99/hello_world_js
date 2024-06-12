5da1cbb21a52ae3de775d34704874595cb519980
Anabella <anabella@amatek.ru>
2024-06-12 23:15:07 +0300

    TEST

---
+++ hello_world.java
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Internal javascript</title>
+</head>
+<body>
+  <h1>Using internal javascript</h1>
+  
+  <!-- script goes here -->
+  <script>
+    console.log("Hello World!");
+  </script>
+</body>
+</html>
